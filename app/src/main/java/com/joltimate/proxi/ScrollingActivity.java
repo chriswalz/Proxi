69efcecca293fd14b2a0d42ecfd309a7148739c3
quaintcow <zlaw777@gmail.com>
2015-10-05 14:22:44 -0400

    added unnecessary comment to scrolling activity

--- app/src/main/java/com/joltimate/proxi/ScrollingActivity.java
+++ app/src/main/java/com/joltimate/proxi/ScrollingActivity.java
@@ -6,12 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.view.View;
 import android.widget.ListView;
 
 import com.firebase.client.DataSnapshot;
@@ -46,7 +43,7 @@ public class ScrollingActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 if ( listView != null){
-                    ArrayList<String> singleStringList = new ArrayList<String>();
+                    ArrayList<String> singleStringList = new ArrayList<>();
                     singleStringList.add(snapshot.getValue().toString());
                     changeList(listView, singleStringList);
                 }
@@ -67,6 +64,8 @@ public class ScrollingActivity extends AppCompatActivity {
         builder.setPositiveButton("OK", null);
         builder.setNegativeButton("Cancel", null);
         builder.show();
+
+        // lolo lo l
     }
 
     @Override
@@ -90,7 +89,7 @@ public class ScrollingActivity extends AppCompatActivity {
                 "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
                 "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
                 "Android", "iPhone", "WindowsMobile" };
-        final ArrayList<String> list = new ArrayList<String>();
+        final ArrayList<String> list = new ArrayList<>();
         for (int i = 0; i < values.length; ++i) {
             list.add(values[i]);
         }
